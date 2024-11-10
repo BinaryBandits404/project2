51f3c7efb08d89e50602217bd1c9cd7b5b5c61b9
Tanya Grace S. Gonzaga <tanyangqty25@gmail.com>
2024-11-10 18:58:44 +0800

    svelte installation and dependency

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import flowbitePlugin from "flowbite/plugin";
+
+import type { Config } from "tailwindcss";
+
+export default {
+	content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+	darkMode: "class",
+theme: {
+		extend: {
+			colors: {
+				primary: {
+				},
+			},
+		},
+	},
+
+	plugins: [flowbitePlugin],
+} as Config;
